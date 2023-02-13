b81feb5a61b56a93c3a2bfc2a01c9b1320d5c709
Andrea Duque <andreacduque21@gmail.com>
2023-02-13 10:54:05 +0100

    actualizo codigo sql y creacion csv nuevo para el data warehouse

--- airbnb.sql
+++ airbnb.sql
@@ -1,76 +1,41 @@
+
+-- La creación de base de datos la hicimos con el programa pgAdmin
 --Creamos el esquema
-create schema prueba_csv authorization ckuqrdql;
+create schema airbnb
 
 -- Creamos tabla
 
-create table prueba_csv.airbnb(
-    ID varchar(10),
-    "Host ID" varchar(100),
-    "Host Since" date,
-    "Host Name" varchar (100),
+create table airbnb.airbnb_table(
+    ID integer,
+    Host_ID integer,
+	Host_Name varchar (100),
+    Host_Since date,
+	Host_Location varchar (150),
     Street varchar (500),
-    "Neighbourhood Cleansed" varchar (100),
+    Neighbourhood_Cleansed varchar (100),
+    Neighbourhood_Group_Cleansed varchar (100),
     City varchar (50),
-    State varchar (100),
-    Zipcode varchar(100),
-    "Smart Location" varchar (150),
-    "Country Code" varchar (50),
+    Zipcode varchar(20),
     Country varchar (100),
-    Latitude varchar (20),
-    Longitude varchar (20),
-    "Property Type" varchar (50),
-    "Room Type" varchar (100),
-    Bathrooms varchar (5),
-    Bedrooms varchar (5),
-    Beds varchar (5),
-    "Bed Type" varchar (50),
-    Price  varchar (100),
-    "Number of Reviews" varchar (500),
-    "Cancellation Policy" varchar (100),
-    Geolocation varchar (200)
+    Latitude float,
+    Longitude float,
+    Property_Type varchar (50),
+    Room_Type varchar (100),
+    Accomodates integer,
+    Bathrooms float,
+    Bedrooms float,
+    Beds float,
+    Bed_Type varchar (50),
+    Amenities varchar (500),
+    Price float,
+    Guests_Included integer,
+    Minimum_Nights integer,
+    Maximum_Nights integer,
+    Number_of_Reviews integer,
+    Cancellation_Policy varchar (100),
+    Calculated_Host_Listings_Count float
     );
 
-ALTER ROLE ckuqrdql WITH SUPERUSER 
-
-COPY prueba_csv.airbnb (ID,"Host ID","Host Since", "Host Name", Street,"Neighbourhood Cleansed",
-    City,State,Zipcode,"Smart Location","Country Code",Country,Latitude,Longitude,"Property Type","Room Type",Bathrooms,Bedrooms,Beds,
-    "Bed Type",Price, "Number of Reviews","Cancellation Policy",Geolocation)
-FROM 'C:\Users\maria\Downloads\Proyecto-Final-KC-Data-Grupo-3-main\Proyecto-Final-KC-Data-Grupo-3-main'
-DELIMITER ','
-CSV HEADER;
-
-psql -h <host> -U <user> -d <ckuqrdql> -c \copy <prueba_csv.airbnb> FROM '<C:\Users\maria\Downloads\Proyecto-Final-KC-Data-Grupo-3-main\Proyecto-Final-KC-Data-Grupo-3-main>' with (format csv,header true, delimiter ',');
-
-COPY table TO 'C:\Users\maria\Downloads\Proyecto-Final-KC-Data-Grupo-3-main\Proyecto-Final-KC-Data-Grupo-3-main\airbnb_data_warehouse.csv' DELIMITER ',' CSV HEADER;
-
-copy (select * FROM table) to 'C:\Users\maria\Downloads\Proyecto-Final-KC-Data-Grupo-3-main\Proyecto-Final-KC-Data-Grupo-3-main\airbnb_data_warehouse.csv' delimiter ',' csv header;
-
-\copy prueba_csv.airbnb from 'C:\Users\maria\Downloads\Proyecto-Final-KC-Data-Grupo-3-main\Proyecto-Final-KC-Data-Grupo-3-main\airbnb_data_warehouse.csv' WITH DELIMITER ',' CSV;
-
-BEGIN;
-Copy prueba_csv.airbnb (ID,"Host ID","Host Since", "Host Name", Street,"Neighbourhood Cleansed",
-    City,State,Zipcode,"Smart Location","Country Code",Country,Latitude,Longitude,"Property Type","Room Type",Bathrooms,Bedrooms,Beds,
-    "Bed Type",Price, "Number of Reviews","Cancellation Policy",Geolocation)
-from 'C:\Users\maria\Downloads\Proyecto-Final-KC-Data-Grupo-3-main\Proyecto-Final-KC-Data-Grupo-3-main\airbnb_data_warehouse.csv'
-DELIMITER ','
-CSV HEADER;
-COMMIT;
-
-Copy prueba_csv.airbnb from stdin (Delimiter ‘,’);
-
-COPY  prueba_csv.airbnb (ID,"Host ID","Host Since", "Host Name", Street,"Neighbourhood Cleansed",
-    City,State,Zipcode,"Smart Location","Country Code",Country,Latitude,Longitude,"Property Type","Room Type",Bathrooms,Bedrooms,Beds,
-    "Bed Type",Price, "Number of Reviews","Cancellation Policy",Geolocation)
-FROM 'C:\Users\maria\Downloads\Proyecto-Final-KC-Data-Grupo-3-main\Proyecto-Final-KC-Data-Grupo-3-main\airbnb_data_warehouse.csv'  
-WITH
-DELIMITER  ',' 
-
-select *
-from prueba_csv.airbnb
-
-select city 
-from prueba_csv.airbnb a 
-
-select "Host Since"  
-from prueba_csv.airbnb a 
-
+--Importamos los datos del CSV a través de pgAdmin con la funcion Import Data ya que al momento de ejecutar el código nos daba el siguiente error: 
+--SQL Error [42501]: ERROR: must be superuser or a member of the pg_read_server_files role to COPY from a file
+--Hint: Anyone can COPY to stdout or from stdin. psql's \copy command also works for anyone.
